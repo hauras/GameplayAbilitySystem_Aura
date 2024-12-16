ad56f5361986a1b7f6859d0d733046e465573afb
haura <gg1592@naver.com>
2024-12-16 16:40:40 +0900

    enemy interface 생성 및 가상 함수 재정의

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -4,14 +4,19 @@
 
 #include "CoreMinimal.h"
 #include "Character/AuraCharacter.h"
+#include "Interaction/EnemyInterface.h"
 #include "AuraEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class AURA_API AAuraEnemy : public AAuraCharacter
+class AURA_API AAuraEnemy : public AAuraCharacter, public IEnemyInterface
 {
 	GENERATED_BODY()
+
+public:
+	virtual void HighlightActor() override; // 순수 가상 함수기 때문 반드시 둘다 재정의 해야함
+	virtual void UnHighlightActor() override; // 그렇지 않으면 인스턴스를 생성할 수 없다. 
 	
 };
